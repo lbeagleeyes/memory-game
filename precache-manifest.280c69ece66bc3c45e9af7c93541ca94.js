cdcb908fcbcba9150833d6ed8b45cf8c50a0fb2f
Laura B <laurabermudezg@gmail.com>
2019-07-10 13:37:39 -0700

    Updates

---
+++ precache-manifest.280c69ece66bc3c45e9af7c93541ca94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc9eb2cecc73d8a168c8641fd74c514",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "cceed16a27b235bd8482",
+    "url": "/memory-game/static/css/main.ea09d28f.chunk.css"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/memory-game/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "cceed16a27b235bd8482",
+    "url": "/memory-game/static/js/main.098133ed.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
